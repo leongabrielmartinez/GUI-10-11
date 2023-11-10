256915e5eb8a5b303ef92bd3d8ee7ef8ef704769
Gabriel <gabrielmartinezdev@gmail.com>
2023-11-10 19:41:42 -0300

    first

---
+++ test.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+from pygame.locals import *
+from GUI_form_prueba import FormPrueba
+
+pygame.init()
+WIDTH = 1200
+HEIGHT = 600
+FPS = 60
+
+reloj = pygame.time.Clock()
+pantalla = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+#CREAR FORMULARIO 
+form_prueba = FormPrueba(pantalla, 200, 100, 900, 350, "cyan", "yellow", 3, True)
+#fromulario activo es que se muetre en pantalla
+
+while True:
+    reloj.tick(FPS)
+    eventos = pygame.event.get()
+    for event in eventos:
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pantalla.fill("Black")
+    form_prueba.update(eventos)
+    
+    
+    pygame.display.flip()
